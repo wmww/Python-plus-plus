bb7830529169ec8bb87244c37c2698de16d745e9
William W Wold <wilcoze@gmail.com>
2017-05-14 16:58:54 -0700

    cleaned up a bit

--- main.cpp
+++ main.cpp
@@ -1,43 +1,61 @@
-#include <iostream>
 
+#define comment /// mscl
+
+#include <iostream>
 #define print(arg) {std::cout << arg << std::endl;}
+#define var() auto
+#define ccp const char *
+
+
+#define comment /// function
 
 #define funcMacro(head, body) auto head; body;}
 #define def funcMacro(
 #define lastArg int dummyArg) {class LocalClass{const int a;LocalClass(
-#define startFunc() a(0){}};
 
-#define dummyGlobalStr1 const char * dummyGlobalStr1_
-#define dummyGlobalStr2 const char * dummyGlobalStr2_
-#define dummyGlobalStr3 const char * dummyGlobalStr3_
-#define dummyGlobalStr4 const char * dummyGlobalStr4_
-#define dummyGlobalStr5 const char * dummyGlobalStr5_
-#define dummyGlobalStr6 const char * dummyGlobalStr6_
-dummyGlobalStr1 = """ "; /* " """
-def startFunc():
+
+#define comment /// function start start
+	
+#define start() a(0){}};
+
+#define start_dummy_0 ccp start_dummy_0_
+start_dummy_0 = """ ";/* " """
+def start():
 	pass
-""" "; */ dummyGlobalStr2 = " """;
+end_dummy_3 = """ */
+const char *start_dummy_1 = " """;
+	
+
+#define comment ///footer
+
+#define end_dummy_0 ccp end_dummy_0_
+#define end_dummy_1 ccp end_dummy_1_
+#define end_dummy_2 ccp end_dummy_2_
+#define end_dummy_3 ccp end_dummy_3_
+
+#define CPP_MAIN int main(int argc, char ** argv) {run(0); return 0;}
 
 def myFuncName(lastArg):
-	startFunc()
+	start()
 	""" " , //" """
 	print("hay");
 	""" ") //" """
 	
 def run(lastArg):
-	startFunc()
+	start()
 	""" " , //" """
 	myFuncName(0);
 	""" ") //" """
 
-dummyGlobalStr3 = """ ";
-int main(int argc, char ** argv)
-{
-	run(0);
-	return 0;
-}
-dummyGlobalStr4 = ""; /*"""
+#define comment // boilerplate footer
+
+end_dummy_0 = """ ";
+CPP_MAIN;
+end_dummy_1 = " """;
+
+end_dummy_2 = """ ";/* " """
 run(0)
-"""; */ dummyGlobalStr5 = " """;
+end_dummy_3 = """ */
+end_dummy_3 = " """;
 
 

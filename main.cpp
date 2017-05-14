88a0bc0821ed967c98c46b8e7fda97c352834171
William W Wold <wilcoze@gmail.com>
2017-05-14 14:51:07 -0700

    initial commit

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+#include <vector>
+using std::vector;
+
+vector<int> stringToEmptyIntVector(const char * str) {return vector<int>();}
+
+#define macroFunc(head, body) auto head; body;}
+
+#define def macroFunc(
+  
+#define defaultArg ) {class LocalClass{const int a;LocalClass(
+
+#define startFunc() a(0){}};
+
+#define closePeren )
+
+def jhjkhjjk(defaultArg):
+	startFunc()
+	""" " , //" """
+	cout << "hay" << endl;
+	cout << "yo" << endl;
+	""" ") //" """
+
+int main(int argc, char ** argv)
+{
+  jhjkhjjk();
+  return 0;
+}
+
+

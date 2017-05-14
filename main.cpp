d20927f50e7259f0a32004290fd803fed980af2c
William W Wold <wilcoze@gmail.com>
2017-05-14 16:16:57 -0700

    got MVP working

--- main.cpp
+++ main.cpp
@@ -1,33 +1,43 @@
 #include <iostream>
-using std::cout;
-using std::endl;
 
-#include <vector>
-using std::vector;
+#define print(arg) {std::cout << arg << std::endl;}
 
-vector<int> stringToEmptyIntVector(const char * str) {return vector<int>();}
-
-#define macroFunc(head, body) auto head; body;}
-
-#define def macroFunc(
-  
-#define defaultArg ) {class LocalClass{const int a;LocalClass(
-
+#define funcMacro(head, body) auto head; body;}
+#define def funcMacro(
+#define lastArg int dummyArg) {class LocalClass{const int a;LocalClass(
 #define startFunc() a(0){}};
 
-#define closePeren )
+#define dummyGlobalStr1 const char * dummyGlobalStr1_
+#define dummyGlobalStr2 const char * dummyGlobalStr2_
+#define dummyGlobalStr3 const char * dummyGlobalStr3_
+#define dummyGlobalStr4 const char * dummyGlobalStr4_
+#define dummyGlobalStr5 const char * dummyGlobalStr5_
+#define dummyGlobalStr6 const char * dummyGlobalStr6_
+dummyGlobalStr1 = """ "; /* " """
+def startFunc():
+	pass
+""" "; */ dummyGlobalStr2 = " """;
 
-def jhjkhjjk(defaultArg):
+def myFuncName(lastArg):
 	startFunc()
 	""" " , //" """
-	cout << "hay" << endl;
-	cout << "yo" << endl;
+	print("hay");
+	""" ") //" """
+	
+def run(lastArg):
+	startFunc()
+	""" " , //" """
+	myFuncName(0);
 	""" ") //" """
 
+dummyGlobalStr3 = """ ";
 int main(int argc, char ** argv)
 {
-  jhjkhjjk();
-  return 0;
+	run(0);
+	return 0;
 }
+dummyGlobalStr4 = ""; /*"""
+run(0)
+"""; */ dummyGlobalStr5 = " """;
 
 

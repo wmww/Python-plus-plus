65a4cd737f1aaf8d365c802fa492e159e1663550
William W Wold <wilcoze@gmail.com>
2017-05-14 17:49:43 -0700

    added array support

--- main.cpp
+++ main.cpp
@@ -21,24 +21,23 @@
 #define is_true(in) (in) {for (int i
 #define block() std::vector<int>()) {};
 
+#define comment /// array
+
+#define Array(type) std::vector<decltype(type)>();
+#define len(ary) ary.size()
+#define append push_back
+
 #define comment /// dummy python functions
 
 #define start_dummy_0 ccp start_dummy_0_
 start_dummy_0 = """ ";/* " """
-def func():
-	pass
-
-def var():
-	pass
-
-def block():
-	pass
-
-def is_true(condition):
-	return condition
-	
+def func(): pass
+def var(): pass
+def block(): pass
+def is_true(condition): return condition
+def Array(dummy): return []
 end_dummy_3 = """ */
-const char *start_dummy_1 = " """;
+ccp start_dummy_1 = " """;
 	
 
 #define comment ///footer
@@ -55,36 +54,35 @@ def myFuncName(lastArg):
 	""" " , //" """
 	
 	var()
-	a = 8;
+	a = Array(0);
 	
-	while is_true(a > 0):
-		block()
-		print(a)
-		a = a - 1;
+	a.append(3);
+	a.append(9);
+	a.append(147);
+	
+	var()
+	i = 0;
+	
+	while is_true(i < len(a)):
+		block();
+		print(a[i]);
+		i = i + 1;
 		""" ";}//" """
 	
-	print(a);
 	print("hay");
 	
 	""" ")//" """
 	
 def run(lastArg):
 	func()
-	""" " , //" """
+	""" ",//" """
 	
 	myFuncName(0);
 	
-	""" ") //" """
+	""" ")//" """
 
 #define comment // boilerplate footer
-
-end_dummy_0 = """ ";
-CPP_MAIN;
-end_dummy_1 = " """;
-
-end_dummy_2 = """ ";/* " """
-run(0)
-end_dummy_3 = """ */
-end_dummy_3 = " """;
+end_dummy_0 = """ "; CPP_MAIN; end_dummy_1 = " """;
+end_dummy_2 = """ ";/* " """; run(0); end_dummy_3 = """ */ end_dummy_3 = " """;
 
 

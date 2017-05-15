b95df361c44a87893d0fc905a00e20e8b0eda3d8
William W Wold <wilcoze@gmail.com>
2017-05-14 17:24:41 -0700

    implemented while loops

--- main.cpp
+++ main.cpp
@@ -2,9 +2,10 @@
 #define comment /// mscl
 
 #include <iostream>
+#include <vector>
 #define print(arg) {std::cout << arg << std::endl;}
 #define var() auto
-#define start() a(0){}};
+#define func() a(0){}};
 #define ccp const char *
 
 
@@ -15,15 +16,26 @@
 #define lastArg int dummyArg) {class LocalClass{const int a;LocalClass(
 
 
+#define comment /// while loop
+
+#define is_true(in) (in) {for (int i
+#define block() std::vector<int>()) {};
+
 #define comment /// dummy python functions
 
 #define start_dummy_0 ccp start_dummy_0_
 start_dummy_0 = """ ";/* " """
-def start():
+def func():
 	pass
 
 def var():
 	pass
+
+def block():
+	pass
+
+def is_true(condition):
+	return condition
 	
 end_dummy_3 = """ */
 const char *start_dummy_1 = " """;
@@ -39,19 +51,29 @@ const char *start_dummy_1 = " """;
 #define CPP_MAIN int main(int argc, char ** argv) {run(0); return 0;}
 
 def myFuncName(lastArg):
-	start()
+	func()
 	""" " , //" """
+	
 	var()
 	a = 8;
-	a = 4+7;
+	
+	while is_true(a > 0):
+		block()
+		print(a)
+		a = a - 1;
+		""" ";}//" """
+	
 	print(a);
 	print("hay");
-	""" ") //" """
+	
+	""" ")//" """
 	
 def run(lastArg):
-	start()
+	func()
 	""" " , //" """
+	
 	myFuncName(0);
+	
 	""" ") //" """
 
 #define comment // boilerplate footer
